ecb0a5cb0177113009cabab74234145934e3c1de
CrazyLab1001 <cramerkj@mail.uc.edu>
2025-02-01 13:26:34 -0500

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        double itemPrice = 128.39; // this is actually the market price of a card I'm looking at. T-T I love being a card collector.
-        double SALES_TAX = .05;
-        double itemPriceWithTax = itemPrice + (itemPrice * SALES_TAX);
-        System.out.println("If the price of your item is " + itemPrice + ", then the price of your item after sales tax is " + itemPriceWithTax + ".");
-
+        System.out.println("Hello World!");
     }
 }
